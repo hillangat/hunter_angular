956f845707a4f83e3702bd078bff64a47b821234
Kip <hillangat@gmail.com>
2017-07-27 23:41:30 -0400

    Testing commit right from visual studio code!!

--- Hunter/src/app/components/home/home.ts
+++ Hunter/src/app/components/home/home.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { HunterTableConfig } from '../../beans/hunter-table-configs';
 import { taskHistory } from '../../data/mocked-task-history';
+import { ModalDirective } from 'ngx-bootstrap/modal';
 
 @Component({
     moduleId:module.id,
@@ -12,6 +13,8 @@ export class HomeComponent{
 
   currFunc:string = null;
   currDataId:number = null;
+  index:number;
+  popupModalTitle:string = 'Delete Task History';
 
 
   headers:Array<HunterTableConfig> = [
@@ -20,7 +23,7 @@ export class HomeComponent{
     {"index":2, "headerId":'eventMessage',"dataId":'historyId',"displayName":'Event Message',"sortable":true,"show":true,"bootstrapIconName":'remove',"dataType":'string','currentOrder':false,'isActionCol':false,'actionColIconName':null },
     {"index":3, "headerId":'eventDate',"dataId":'historyId',"displayName":'Event Date',"sortable":true,"show":true,"bootstrapIconName":'calendar',"dataType":'string','currentOrder':false,'isActionCol':false,'actionColIconName':null },
     {"index":4, "headerId":'eventUser',"dataId":'historyId',"displayName":'Event User',"sortable":true,"show":true,"bootstrapIconName":'user',"dataType":'string','currentOrder':false,'isActionCol':false,'actionColIconName':null },
-    {"index":5, "headerId":'eventStatus',"dataId":'historyId',"displayName":'Event Status',"sortable":true,"show":true,"bootstrapIconName":'clock',"dataType":'string','currentOrder':true,'isActionCol':false,'actionColIconName':null },
+    {"index":5, "headerId":'eventStatus',"dataId":'historyId',"displayName":'Status',"sortable":true,"show":true,"bootstrapIconName":'clock',"dataType":'string','currentOrder':true,'isActionCol':false,'actionColIconName':null },
     {"index":5, "headerId":'edit',"dataId":'historyId',"displayName":'Edit',"sortable":false,"show":false,"bootstrapIconName":'edit',"dataType":'string','currentOrder':false,'isActionCol':true,'actionColIconName':'pencil' },
     {"index":5, "headerId":'delete',"dataId":'historyId',"displayName":'Delete',"sortable":false,"show":false,"bootstrapIconName":'remove',"dataType":'string','currentOrder':false,'isActionCol':true,'actionColIconName':'remove' }
   ]
@@ -33,7 +36,23 @@ export class HomeComponent{
         {"taskId":4,"evenName":"Clone","eventMessage":"Successfully cloned task. New task ( 9 ) for new user ( admin )","historyId":76,"eventDate":"2017-05-27 17:55","eventUser":"mathash","eventStatus":"Success"},
         {"taskId":4,"evenName":"Clone","eventMessage":"Failed to clone task.Client already has a task with this task name.","historyId":75,"eventDate":"2017-05-27 17:55","eventUser":"mathash","eventStatus":"Failed"},
         {"taskId":4,"evenName":"Clone","eventMessage":"Successfully cloned task. New task ( 8 ) for new user ( admin )","historyId":74,"eventDate":"2017-05-27 17:55","eventUser":"mathash","eventStatus":"Success"},
-        {"taskId":4,"evenName":"Process","eventMessage":"Successfully processed task!","historyId":58,"eventDate":"2017-05-26 21:26","eventUser":"admin","eventStatus":"Success"}        
+        {"taskId":4,"evenName":"Process","eventMessage":"Successfully processed task!","historyId":58,"eventDate":"2017-05-26 21:26","eventUser":"admin","eventStatus":"Success"},        
+        {"taskId":4,"evenName":"Change Status","eventMessage":"Failed to clone task.Client already has a task with this task name.","historyId":59,"eventDate":"2017-05-27 17:55","eventUser":"mathash","eventStatus":"Failed"},
+        {"taskId":4,"evenName":"Clone","eventMessage":"Successfully cloned task. New task ( 11 ) for new user ( admin )","historyId":60,"eventDate":"2017-05-27 17:55","eventUser":"mathash","eventStatus":"Success"},
+        {"taskId":4,"evenName":"Clone","eventMessage":"Successfully cloned task. New task ( 10 ) for new user ( admin )","historyId":61,"eventDate":"2017-05-27 17:55","eventUser":"mathash","eventStatus":"Success"},
+        {"taskId":4,"evenName":"Clone","eventMessage":"Failed to clone task.Client already has a task with this task name.","historyId":62,"eventDate":"2017-05-27 17:55","eventUser":"mathash","eventStatus":"Failed"},
+        {"taskId":4,"evenName":"Clone","eventMessage":"Successfully cloned task. New task ( 9 ) for new user ( admin )","historyId":63,"eventDate":"2017-05-27 17:55","eventUser":"mathash","eventStatus":"Success"},
+        {"taskId":4,"evenName":"Clone","eventMessage":"Failed to clone task.Client already has a task with this task name.","historyId":64,"eventDate":"2017-05-27 17:55","eventUser":"mathash","eventStatus":"Failed"},
+        {"taskId":4,"evenName":"Clone","eventMessage":"Successfully cloned task. New task ( 8 ) for new user ( admin )","historyId":65,"eventDate":"2017-05-27 17:55","eventUser":"mathash","eventStatus":"Success"},
+        {"taskId":4,"evenName":"Process","eventMessage":"Successfully processed task!","historyId":66,"eventDate":"2017-05-26 21:26","eventUser":"admin","eventStatus":"Success"},        
+        {"taskId":4,"evenName":"Change Status","eventMessage":"Failed to clone task.Client already has a task with this task name.","historyId":67,"eventDate":"2017-05-27 17:55","eventUser":"mathash","eventStatus":"Failed"},
+        {"taskId":4,"evenName":"Clone","eventMessage":"Successfully cloned task. New task ( 11 ) for new user ( admin )","historyId":68,"eventDate":"2017-05-27 17:55","eventUser":"mathash","eventStatus":"Success"},
+        {"taskId":4,"evenName":"Clone","eventMessage":"Successfully cloned task. New task ( 10 ) for new user ( admin )","historyId":69,"eventDate":"2017-05-27 17:55","eventUser":"mathash","eventStatus":"Success"},
+        {"taskId":4,"evenName":"Clone","eventMessage":"Failed to clone task.Client already has a task with this task name.","historyId":70,"eventDate":"2017-05-27 17:55","eventUser":"mathash","eventStatus":"Failed"},
+        {"taskId":4,"evenName":"Clone","eventMessage":"Successfully cloned task. New task ( 9 ) for new user ( admin )","historyId":71,"eventDate":"2017-05-27 17:55","eventUser":"mathash","eventStatus":"Success"},
+        {"taskId":4,"evenName":"Clone","eventMessage":"Failed to clone task.Client already has a task with this task name.","historyId":72,"eventDate":"2017-05-27 17:55","eventUser":"mathash","eventStatus":"Failed"},
+        {"taskId":4,"evenName":"Clone","eventMessage":"Successfully cloned task. New task ( 8 ) for new user ( admin )","historyId":73,"eventDate":"2017-05-27 17:55","eventUser":"mathash","eventStatus":"Success"},
+        {"taskId":4,"evenName":"Process","eventMessage":"Successfully processed task!","historyId":74,"eventDate":"2017-05-26 21:26","eventUser":"admin","eventStatus":"Success"}        
     ];
 
   
@@ -41,24 +60,31 @@ export class HomeComponent{
 
   constructor( ){}
 
+    createNewAction( dataBeanId:string ){      
+      if( dataBeanId == 'Task History' ){
+        this.popupModalTitle = "Create New " + dataBeanId;
+        this.showModal();
+      }
+    }
+
     handleGridAction(params:any[]){
       
       
       this.currFunc   = params[0]; 
       this.currDataId = params[1];
+      this.index      = -1;     
       
-      let index    = -1;
 
       if( this.currFunc == 'delete' ){
         for(var i=0; i<this.hunterTableData.length; i++ ){
           var historyId = this.hunterTableData[i].historyId;
           if( historyId == this.currDataId ){
             this.currDataId = historyId;
-            index = i;
+            this.index = i;
           }
         }
-        if( index != -1 ){
-          this.hunterTableData.splice( index, 1 );
+        if( this.index != -1 ){
+          this.showModal();
         }else{
           console.error( 'No index to be deleted found!! Bad data! funcName = ' + this.currFunc + ', dataId = ' + this.currDataId );
         }
@@ -66,6 +92,34 @@ export class HomeComponent{
         alert('Opening edit....');
       }
     }
+
+    deleteSelectedRow(){
+      this.hunterTableData.splice( this.index, 1 );
+      this.hideModal();
+    }
+
+
+  @ViewChild('autoShownModal') 
+  public autoShownModal:ModalDirective;
+
+  public isModalShown:boolean = false;
+ 
+  public showModal():void {
+    this.isModalShown = true;
+  }
+ 
+  public hideModal():void {
+    this.autoShownModal.hide();
+  }
+ 
+  public onHidden():void {
+    this.isModalShown = false;
+  }
+
+
+
+
+
   
 
 }
--- Hunter/src/app/components/hunter-table-widgets/hunter-table-config.ts
+++ Hunter/src/app/components/hunter-table-widgets/hunter-table-config.ts
@@ -1,44 +1,251 @@
-import { Component,Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, OnInit, EventEmitter,ElementRef } from '@angular/core';
 import { taskHistory } from '../../data/mocked-task-history';
 
-@Component({  
-  moduleId:module.id,
-  selector: 'hunter-table-config',
-  templateUrl: 'hunter-table-config.html',
-  styleUrls: ['hunter-table-config.css'],
-  inputs: [ 'headers','hunterTableData' ]
+@Component({
+    moduleId: module.id,
+    selector: 'hunter-table-config',
+    templateUrl: 'hunter-table-config.html',
+    styleUrls: ['hunter-table-config.css'],
+    inputs: ['headers', 'hunterTableData']
 })
-export class HunterTableConfig {
+export class HunterTableConfig implements OnInit {
 
-     @Output() handleGridAction = new EventEmitter<any[]>();
+    @Output() handleGridAction = new EventEmitter<any[]>();
+    @Output() createNewAction = new EventEmitter<string>();
 
-    getBootstrapClass( name:string ){
+    @Input('hunterTableData') hunterTableData:any[];
+    private visibleHunterTableData:any[];
+    
+    private overIsOn: boolean = false;
+    private selItemsPerPage:number = 1;
+    private displaySelItemsPerPage:string = "1";
+    private hasNewRowButton:boolean = true;
+    private dataBeanName:string = "Task History";
+    private calculatedPageNumbers:number = 0;
+    private calculatedPageNumberArray:number[] = [];
+    private visiblePageNumbers:number[] = [];
+    private maxVisiblePageNos:number = 3;
+    private totalRowNum:number = 0;
+    private currentPageNo:number = 1;
+    private startIndex:number = 0 ;
+    private endIndex:number = 0 
+
+    
+
+
+    constructor( private ref: ElementRef ){
+
+    }
+
+    ngOnInit(){
+        this.initializeDataGrid();
+    }
+
+    initializeDataGrid(){
+        this.calculatePageNumbers();
+        this.redrawBubblePages();
+        this.updateVisibleHunterTableData();
+    }
+
+    calculatePageNumbers(){
+        
+        this.totalRowNum = this.hunterTableData.length;
+
+        /** If the user selects 'All', then the page number is equal to 1 */
+        if( this.selItemsPerPage == 0 ){
+            this.calculatedPageNumbers = 1;
+            console.log( this.calculatedPageNumbers );
+            this.redrawBubblePages();
+            return;
+        }
+        if( this.selItemsPerPage >= this.totalRowNum ){
+            this.calculatedPageNumbers = 1;
+            this.redrawBubblePages();       
+            return;
+        }
+
+        
+        /** If there is a decimal, the page no is +1 */
+        let pageNo  = this.totalRowNum / this.selItemsPerPage; // 8 divide by 2 = 4
+        let decimal = pageNo - Math.floor(pageNo);        
+        this.calculatedPageNumbers = decimal >= 0 ? (pageNo++) : pageNo;
+
+        /** There has gotta be at least one page */
+        this.calculatedPageNumbers = this.calculatedPageNumbers < 1 ? 1 : this.calculatedPageNumbers;
+    }
+
+    setItemsPerPage( _selItemsPerPage ){
+        this.selItemsPerPage = _selItemsPerPage;      
+        this.displaySelItemsPerPage = _selItemsPerPage == 0 ? "All" : _selItemsPerPage + "";       
+        this.currentPageNo = 1; 
+        this.initializeDataGrid();
+    }
+
+    
+
+    updateVisibleHunterTableData(){
+        
+        if( this.calculatedPageNumbers == 1 ){
+            this.startIndex = 0;
+            this.endIndex =  this.hunterTableData.length - 1;
+            this.visibleHunterTableData = this.hunterTableData;
+            this.startIndex = 0;
+            this.endIndex = this.hunterTableData.length;
+            return;
+        }      
+
+        this.visibleHunterTableData = [];
+
+        this.startIndex = ((this.currentPageNo-1)*this.selItemsPerPage);
+        this.endIndex   = this.startIndex + this.selItemsPerPage;
+
+        for( var i = this.startIndex; i<this.endIndex; i++ ){
+            if( this.hunterTableData.length > i ){
+                this.visibleHunterTableData.push(this.hunterTableData[i]);
+            }else{
+                break;
+            }
+        }
+    }
+    
+    getStyleForSelPageCircle( pageNo ){        
+        return this.currentPageNo == pageNo ? '#D6F0F2' : '';
+    }
+    
+    setCurrentPage( selPageNo ){
+        this.currentPageNo = selPageNo;  
+        //this.updateVisibleHunterTableData();
+        this.initializeDataGrid();                   
+    }
+
+    redrawBubblePages(){
+        
+        this.calculatedPageNumberArray = [];            
+
+        for(var i=0; i<this.calculatedPageNumbers; i++){
+            this.calculatedPageNumberArray.push(i+1);
+        }
+
+        this.visiblePageNumbers = [];
+        
+        var 
+        totalPages  = this.calculatedPageNumberArray.length,
+        maxBlocks   = totalPages/this.maxVisiblePageNos,
+        decimal     = maxBlocks - Math.floor(maxBlocks),
+        maxBlocks   = decimal > 0 ? maxBlocks : maxBlocks + 1,
+        minPage     = 1,
+        maxPage     = minPage + this.maxVisiblePageNos - 1;
+
+        console.log( "currentPageNo = " + this.currentPageNo );
+
+        let isWithinRange = this.currentPageNo >= minPage;
+        let counter = 0;
+
+        while( isWithinRange ){
+            let nextMinPage   = maxPage + 1;
+            let nextMaxPage   = nextMinPage + this.maxVisiblePageNos - 1;
+            console.log( "Next minPage = " + minPage );
+            console.log( "Next maxPage = " + maxPage );
+            isWithinRange    = this.currentPageNo >= nextMinPage;
+            if( isWithinRange ){
+                minPage = nextMinPage;
+                maxPage = nextMaxPage;
+            }                   
+        }
+
+        console.log( "minPage = " + minPage );
+        console.log( "maxPage = " + maxPage );
+        
+        for (var i = minPage; i <= maxPage; i++) {
+            if (this.calculatedPageNumberArray.length >= i)
+                this.visiblePageNumbers.push(i);
+        }
+
+        console.log( JSON.stringify( this.visiblePageNumbers ) );       
+    }
+
+        
+    getBootstrapClass(name: string) {
         return 'glyphicon glyphicon-' + name;
     }
 
-    getDatumForHeader(header:HunterTableConfig, row:any){
-        let fieldId  = header['headerId']; //history ID
+    getDatumForHeader(header: HunterTableConfig, row: any) {
+        let fieldId = header['headerId']; //history ID
         let value = row[fieldId]; // 49484
     }
 
-    getRowDataId(datum, header){
-        if( datum == null || datum.length == 0 ){
-            console.log( 'getRowDataId empty datum argument'  );
+    getRowDataId(datum, header) {
+        if (datum == null || datum.length == 0) {
+            console.log('getRowDataId empty datum argument');
         }
-        if( header == null || header.length == 0 ){
-            console.log( 'getRowDataId empty header argument'  );
+        if (header == null || header.length == 0) {
+            console.log('getRowDataId empty header argument');
         }
         return datum[header.dataId]
     }
 
-    onClickButton( funcName:string, dataId:any ){
+    onClickButton(funcName: string, dataId: any) {
         console.log(funcName);
         this.handleGridAction.emit([funcName, dataId]);
+        this.initializeDataGrid();
+    }
+
+    removeOverlay() {
+        this.overIsOn = false;
+    }
+
+    showOverlay() {      
+        this.overIsOn = true;  
+        setTimeout(() => {
+           this.overIsOn = false;
+        }, 1500);
     }
 
     
     
+    showFilterDropdown(){        
+        //
+    }
 
-    
+    displayNewCreationWidget(){        
+        this.createNewAction.emit(this.dataBeanName);
+        this.initializeDataGrid();
+    }
+
+    goToPageOne(){
+        this.currentPageNo = 1;
+        this.initializeDataGrid();
+    }
+
+    goToPreviousPage(){
+        if( this.currentPageNo > 1 ){
+            this.currentPageNo--;
+        }else{
+            this.currentPageNo = 1;
+        }
+        this.initializeDataGrid();
+    }
+
+    goToNextPage(){
+        if( this.currentPageNo < this.calculatedPageNumbers ){
+            this.currentPageNo++;
+        }else{
+            this.currentPageNo = this.currentPageNo;
+        }        
+        this.initializeDataGrid();       
+    }
+
+    goToLastPage(){
+        this.currentPageNo = this.calculatedPageNumberArray[this.calculatedPageNumberArray.length-1];    
+        this.initializeDataGrid();
+    }
+
+
+
+   
+
+
+
+
 
 }
